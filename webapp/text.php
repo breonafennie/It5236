9f29c6e62a9c565b99d59656056a1e9a28b09573
breonafennie <89151186+breonafennie@users.noreply.github.com>
2021-10-19 16:47:02 -0400

    Update text.php

--- webapp/text.php
+++ webapp/text.php
@@ -1,8 +1,12 @@
 <html>
 <body>
-
+<?php str_word_count(string $string, int $format = 0, ?string $characters = null): array|int; 
+        echo "Your word count is int " ?>
+<?php
+    $length = strlen($_POST['textbox']);
+    echo "Your Character count is:  $length.";
+?>
 Welcome <?php echo $_GET["textbox"]; ?><br>
 Your word count is: <?php echo $_GET["textbox"]; ?>
-
 </body>
 </html>
