dc5a6fc265110716845d1d4611d16b80d6cd852f
breonafennie <89151186+breonafennie@users.noreply.github.com>
2021-10-19 15:53:08 -0400

    Update index.php

---
+++ webapp/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Simple Web app</title>
+  <meta name="description" content="A simple web app for IT 5236 .">
+  <meta name="author" content="Breona Fennie">
+  
+  <form action="text.php" method="post">
+<input type="text" name="name"><br>
+<input type="enter">
+</form>
+    
+   </head>
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
