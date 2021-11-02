3eb1070ca49f7502c068e3734a84391cd1ffd3e7
breonafennie <89151186+breonafennie@users.noreply.github.com>
2021-11-02 16:53:09 -0400

    Create demo.php

---
+++ webapp/demo.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "it5236webserver";
+$username = "webapp";
+$password = "student";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
